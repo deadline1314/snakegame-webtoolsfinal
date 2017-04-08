f9e713ed02ee6c0686acf8212b597de2b527f6ca
Ruotian(Kerwin) Wang <wang.ruot@husky.neu.edu>
2017-04-07 23:51:34 -0700

    add bootstrap dependency

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+//import jquery and bootstrap js
+global.jQuery = require('jquery');
+require('bootstrap');
+
+//import bootstrap css
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './index.css';
 
 ReactDOM.render(
