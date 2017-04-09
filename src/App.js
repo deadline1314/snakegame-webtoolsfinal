ce77af966da9f36eb7e5a21f183145f80edcfe1d
Ruotian(Kerwin) Wang <wang.ruot@husky.neu.edu>
2017-04-08 19:56:20 -0700

    first workable version

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
+
+//import firebase dependency
+import firebase from 'firebase/app';
+import 'firebase/database';
+
 import './App.css';
-
-
+import ThreadDisplay from "./component/ThreadDisplay";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    //firebase initialization
+    const config = {
+      apiKey: "AIzaSyAkNLDVunC2HTufatelsnIsl8rLElEhuqg",
+      authDomain: "reacttest-a0dd3.firebaseapp.com",
+      databaseURL: "https://reacttest-a0dd3.firebaseio.com",
+      projectId: "reacttest-a0dd3",
+      storageBucket: "reacttest-a0dd3.appspot.com",
+      messagingSenderId: "901167334752"
+    };
+
+    this.app = firebase.initializeApp(config);
+    this.database = this.app.database();
+
+  }
+
+
   render() {
     return (
-      <div>
-        <div className="panel panel-default ">
-          <div className="panel-body">
-            Hello World!
-          </div>
-        </div>
-      </div>
+      <ThreadDisplay database={this.app.database()} />
     );
   }
 }
